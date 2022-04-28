2070e590641556b36cf0dac208fe1412a2172de9
ajit51 <ajitk433@gmail.com>
2022-04-28 12:07:50 +0530

    image serve

--- src/main/java/com/springbootuploadfile/api/controllers/FIleController.java
+++ src/main/java/com/springbootuploadfile/api/controllers/FIleController.java
@@ -5,14 +5,17 @@ import com.springbootuploadfile.api.services.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/files")
@@ -28,14 +31,26 @@ public class FIleController {
     @PostMapping("/upload")
     public ResponseEntity<FileResponse> uploadFIle(
             @RequestParam("image") MultipartFile image
-    ){
+    ) {
         String uploadImage = null;
         try {
             uploadImage = this.fileService.uploadImage(path, image);
         } catch (IOException e) {
-           e.printStackTrace();
+            e.printStackTrace();
             return new ResponseEntity<>(new FileResponse(uploadImage, "Image is not uploaded due to error on server !!"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return new ResponseEntity<>(new FileResponse(uploadImage, "Image is successfully uploaded !!"), HttpStatus.OK);
+    }
+
+    // method to serve file
+    @GetMapping(value = "/profile/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(@PathVariable String imageName, HttpServletResponse response) {
+        try {
+            InputStream resource = fileService.getResource(path, imageName);
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            StreamUtils.copy(resource, response.getOutputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/springbootuploadfile/api/services/FileServiceImpl.java
+++ src/main/java/com/springbootuploadfile/api/services/FileServiceImpl.java
@@ -3,13 +3,12 @@ package com.springbootuploadfile.api.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Service
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
     @Override
     public String uploadImage(String path, MultipartFile file) throws IOException {
 
@@ -18,7 +17,7 @@ public class FileServiceImpl implements FileService{
 
 
         //full path
-        String filePath = path+ File.separator+filename;
+        String filePath = path + File.separator + filename;
 
         //create folder if not exist
         File f = new File(path);
@@ -31,4 +30,16 @@ public class FileServiceImpl implements FileService{
 
         return filename;
     }
+
+    @Override
+    public InputStream getResource(String path, String fileName) throws FileNotFoundException {
+
+        String fullPath = path + File.separator + fileName;
+        InputStream inputStream = new FileInputStream(fullPath);
+
+        //db logic to return inputstream
+        return inputStream;
+    }
+
+
 }
